2c926a170fadaf70a2a9859be9b72e26ca71129c
Collin McNulty <collin.mcnulty@gmail.com>
2017-04-02 00:27:49 -0500

    submission

--- Q2/src/main/scala/edu/gatech/cse6242/Q2.scala
+++ Q2/src/main/scala/edu/gatech/cse6242/Q2.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.functions._
 
 object Q2 {
-
+	case class Edge(src: String, target: String, weight: Int)
 	def main(args: Array[String]) {
     	val sc = new SparkContext(new SparkConf().setAppName("Q2"))
 		val sqlContext = new SQLContext(sc)
@@ -17,8 +17,26 @@ object Q2 {
     	val file = sc.textFile("hdfs://localhost:8020" + args(0))
 		/* TODO: Needs to be implemented */
 
+		val df = file.map(_.split("\t",-1)).map(p => Edge(p(0), p(1), p(2).trim.toInt)).toDF().filter("weight>1")
+		
+		var incoming = df.groupBy("target").sum("weight").as("in").toDF()
+		var incoming2 = incoming.withColumnRenamed("sum(weight)", "in").toDF()
+		
+		var outgoing = df.groupBy("src").sum("weight").as("out")
+		var outgoing2 = outgoing.withColumnRenamed("sum(weight)", "out").toDF()
+		
+		var combine = incoming2.join(outgoing2, $"src" === $"target", "outer")
+		var combined = combine.withColumn("gross", combine("out")-combine("in")).toDF()
+		//combined.select("src", "gross" )
+		
+		var ss = combined.select(combined("src"), combined("gross")).toDF()
+		
+		val tabSeparated = ss.rdd.map(_.mkString("\t"))
+		//tabSeparated.saveAsTextFile("./test.tsv")
+
+
     	// store output on given HDFS path.
     	// YOU NEED TO CHANGE THIS
-    	file.saveAsTextFile("hdfs://localhost:8020" + args(1))
+    	tabSeparated.saveAsTextFile("hdfs://localhost:8020" + args(1))
   	}
 }
--- Q4/src/main/java/edu/gatech/cse6242/Q4.java
+++ Q4/src/main/java/edu/gatech/cse6242/Q4.java
@@ -11,14 +11,93 @@ import java.io.IOException;
 
 public class Q4 {
 
+  public static class Mapper1
+  	extends Mapper<Object, Text, Text, IntWritable> {
+	  
+	  private final static IntWritable one = new IntWritable(1);
+	  private Text tgt = new Text();
+	  
+	  public void map (Object key, Text value, Context context)
+	  throws IOException, InterruptedException {
+		  String line = value.toString();
+		  String tokens[] = line.split("\t");
+		  Text weight = new Text();
+		  Text wordup = new Text();
+		  weight.set(tokens[0]);
+		  if (tokens.length>1){
+			wordup.set(tokens[1]);
+			context.write(wordup, one);
+		  }
+		  context.write(weight, one);
+		  
+	  }
+  }
+  
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable, Text, IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+  
+  public static class Mapper2
+  	extends Mapper<Object, Text, Text, IntWritable> {
+	  
+	  private final static IntWritable one = new IntWritable(1);
+	  private Text tgt = new Text();
+	  
+	  public void map (Object key, Text value, Context context)
+	  throws IOException, InterruptedException {
+		  String line = value.toString();
+		  String tokens[] = line.split("\t");
+		  Text weight = new Text();
+		  weight.set(tokens[1]);
+		  context.write(weight, one);
+		  
+	  }
+  }
+
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "Q4");
+    Job job1 = Job.getInstance(conf, "Q4");
+	
+	//Stuff I wrote for Q1 and modified:
+    job1.setJarByClass(Q4.class);
+    job1.setMapperClass(Mapper1.class);
+    job1.setCombinerClass(IntSumReducer.class);
+    job1.setReducerClass(IntSumReducer.class);
+    job1.setOutputKeyClass(Text.class);
+    job1.setOutputValueClass(IntWritable.class);
+	FileInputFormat.addInputPath(job1, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job1, new Path("intermediate9"));
+    job1.waitForCompletion(true);
+	//
+    
 
-    /* TODO: Needs to be implemented */
+	
+	
 
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    /* 2nd Job */
+	Job job2 = Job.getInstance(conf, "Q4");
+	job2.setJarByClass(Q4.class);
+    job2.setMapperClass(Mapper2.class);
+    job2.setCombinerClass(IntSumReducer.class);
+    job2.setReducerClass(IntSumReducer.class);
+    job2.setOutputKeyClass(Text.class);
+    job2.setOutputValueClass(IntWritable.class);
+    //
+
+    FileInputFormat.addInputPath(job2, new Path("intermediate9" + "/part-r-00000"));
+    FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+    System.exit(job2.waitForCompletion(true) ? 0 : 1);
   }
 }
